bca1b83661baf1fc486cc8e8f1a8c8549f145bcc
Alexander Herrera <alherreras10@gmail.com>
2020-07-22 17:41:22 -0500

    Proceso y Entidad de Spoiler

--- src/java/Proceso/SpoilerProceso.java
+++ src/java/Proceso/SpoilerProceso.java
@@ -5,10 +5,69 @@
  */
 package Proceso;
 
+import Entidad.Spoiler;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Alexander
  */
 public class SpoilerProceso {
+    Connection conn;
+    
+    
+    /*  Constructor de la clase   */
+    public SpoilerProceso(){
+        
+        try{
+            /*  esto necesita estad dentro de un try catch es para buscar ese archivo, para conectarse a una base de datos MySQL   */
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            
+            /*  Es para igualar la variable conn, con los datos de la base de datos a la que nos estamos conectando, necesita 3 parametros: ruta, usuario y contraseña   */
+            this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten-carrots", "root", "1014");
+            
+            /*  para tirar el error generico de que no se conecto a la base de datos   */
+        }catch(SQLException e){
+            System.out.println("Error al conectar: " + e);
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("Error al registrar el driver de MySQL: " + ex);
+        }
+    }
+    
+    
+    public int GuardarSpoiler(Spoiler spoiler){
+        
+        /* Declarando variable con la que se va a ejecutar el resultado de esta clase */
+        int resultado = 0;
+        
+        try{
+            /* Se necesita hacer una instancia de la accion que se va a crear en ese momento   */
+            /* Java tiene una clase ya predefinida para BDD, llamada Statement */
+            /* De esta manera se crea una instancia a la accion en la base de datos. hay que importarla tambien */
+            Statement smtm = conn.createStatement();
+            
+            /* Se define el query que se va a ejecutar en la base de datos */
+            String query = "INSERT INTO spoiler(titulo_spoiler,descripcion_spoiler)";
+                    /* cadena concatenada para poner los VALUES que se van a insertar */
+                   query += "VALUES('"+spoiler.getTitulo_spoiler()+"','"+spoiler.getDescripcion_spoiler()+"')";
+                  
+            /* Se ejecuta el query con la variable que se habia declarado. entre parentesis ira el query que definimos aneriomente */
+            /* se le iguala a excecuteupdate, para hacer una insercion, actualizacion o borrado */
+            resultado = smtm.executeUpdate(query);
+            
+            return resultado;
+        }
+        catch(Exception w){
+            System.out.println("Error al insertar: " + w);
+        }
+        
+        return 0;
+    }
+    
     
 }
