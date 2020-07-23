2ca6f12b53f98413e64046065a328ac2c42b7f96
Luis <luyimiranda07@gmail.com>
2020-07-22 22:08:14 -0500

    Foro inicio jsp

--- web/query/datos_precargados.sql
+++ web/query/datos_precargados.sql
@@ -4,10 +4,16 @@ INSERT INTO usuario (nombre_usuario, email, contrasena, perfil_usuario) VALUES
 ('leo_choco','lionelh@gmail.com','asqwzx', 'https://i.insider.com/5914ce0ee559f11b008b495b?width=1068&format=jpeg'),
 ('jose_sieiro','josesieiro@gmail.com', 'tyghbn21','https://hungarytoday.hu/wp-content/uploads/2020/06/Hide-the-Pain-Harold-prof..jpg');
 
-INSERT INTO spoiler (id_usuario, id_pelicula, titulo_spoiler, descripcion_spoiler) VALUES
-('1', '2', 'El discurso de Rosie en la boda de Alex', 'Prácticamente confesó que Alex era el amor de su vida ahí merengues, en frente de todos, el día que este men se estaba casando... con Bethany Williams.'),
-('1', '3', 'Andy consigue el trabajo que queria', 'Alfinal de la pelicula Miranda la recomiendad y asi obtiene el trabajo'),
-('2','2', 'Rosie y Alex terminan juntos', 'La verdad no hay mucho que comentar al respecto, es como dice el titulo');
+INSERT INTO spoiler (id_usuario, id_pelicula, titulo_spoiler, genero, descripcion_spoiler) VALUES
+('1', '2', 'El discurso de Rosie en la boda de Alex','Romance', 'Prácticamente confesó que Alex era el amor de su vida ahí merengues, en frente de todos, el día que este men se estaba casando... con Bethany Williams.'),
+('1', '3', 'Andy consigue el trabajo que queria', 'Comedia', 'Alfinal de la pelicula Miranda la recomiendad y asi obtiene el trabajo'),
+('2','2', 'Rosie y Alex terminan juntos', 'Romance', 'La verdad no hay mucho que comentar al respecto, es como dice el titulo');
 
 INSERT INTO comentario (id_usuario, id_spoiler, cuerpo_comentario) VALUES 
 ('3', '1', 'Esa es la parte mas triste de la pelicula, aunque ya alfinal terminan juntos');
+
+
+UPDATE spoiler
+SET carrots = RAND()*(500-100)+100;
+
+SELECT * FROM spoiler;
