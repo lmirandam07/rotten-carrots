49a895903c8b967cc7d44c7f71575cadf8b7fc7b
Lionel Henriquez <leo-henriquez@hotmail.es>
2020-07-24 14:47:26 -0500

    merge

--- src/java/Proceso/SpoilerProceso.java
+++ src/java/Proceso/SpoilerProceso.java
@@ -28,11 +28,9 @@ public class SpoilerProceso {
 
             
             /*  Es para igualar la variable conn, con los datos de la base de datos a la que nos estamos conectando, necesita 3 parametros: ruta, usuario y contraseña   */
-<<<<<<< HEAD
+
             this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "lionel");
-=======
-            this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "12345");
->>>>>>> f261aac56879d93e1c360fc61b1a8b8928ad0530
+
 
 
             /*  para tirar el error generico de que no se conecto a la base de datos   */
