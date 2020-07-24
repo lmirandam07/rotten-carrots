7a7332f2ad698de05b1f62d4887b5b0447174268
Jose <joseasieiro@gmail.com>
2020-07-24 00:36:05 -0500

    l

---
+++ src/java/Entidad/Busqueda.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+/**
+ *
+ * @author chebe
+ */
+public class Busqueda {
+    
+    private String titulospoiler;
+    private String descripcionspoiler;
+    
+    
+    
+
+    /**
+     * @return the titulospoiler
+     */
+    public String getTitulospoiler() {
+        return titulospoiler;
+    }
+
+    /**
+     * @param titulospoiler the titulospoiler to set
+     */
+    public void setTitulospoiler(String titulospoiler) {
+        this.titulospoiler = titulospoiler;
+    }
+
+    /**
+     * @return the descripcionspoiler
+     */
+    public String getDescripcionspoiler() {
+        return descripcionspoiler;
+    }
+
+    /**
+     * @param descripcionspoiler the descripcionspoiler to set
+     */
+    public void setDescripcionspoiler(String descripcionspoiler) {
+        this.descripcionspoiler = descripcionspoiler;
+    }
+    
+    
+}
