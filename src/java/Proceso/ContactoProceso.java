37981dda4d6f82d2417f98d9f5e7c8d6cecb8f38
Javier Alejandro Singh <javieralejandrosingh15@hotmail.com>
2020-07-24 00:38:02 -0500

    Actualizar perfil listo

--- src/java/Proceso/ContactoProceso.java
+++ src/java/Proceso/ContactoProceso.java
@@ -27,22 +27,11 @@ public class ContactoProceso {
         try{
             /*  esto necesita estad dentro de un try catch es para buscar ese archivo, para conectarse a una base de datos MySQL   */
             Class.forName("org.mariadb.jdbc.Driver");
-
-<<<<<<< HEAD
           
             /*  Es para igualar la variable conn, con los datos de la base de datos a la que nos estamos conectando, necesita 3 parametros: ruta, usuario y contraseña   */
 
             this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "12345");
-            
-=======
-            /*  Es para igualar la variable conn, con los datos de la base de datos a la que nos estamos conectando, necesita 3 parametros: ruta, usuario y contraseña   */
 
-
-this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "12345");
-
-            /*  Es para igualar la variable conn, con los datos de la base de datos a la que nos estamos conectando, necesita 3 parametros: ruta, usuario y contraseña   */
-
->>>>>>> a197909f70440b3b3d93ccf9c99a7d29756ef6e7
             /*  para tirar el error generico de que no se conecto a la base de datos   */
         }catch(SQLException e){
             System.out.println("Error al conectar: " + e);
--- web/comentarios.jsp
+++ web/comentarios.jsp
@@ -26,14 +26,9 @@
     <%@include file="templates/header.jsp" %>
     <%
      Class.forName("org.mariadb.jdbc.Driver");
-<<<<<<< HEAD
      Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "12345");
      
     Statement stmt = conn.createStatement();
-=======
-     Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "1014");
-   Statement stmt = conn.createStatement();
->>>>>>> 293fddffdaa82b5ba39f73916285fed54653baad
     ResultSet rs = stmt.executeQuery("SELECT nombre_usuario, cuerpo_comentario, carrots_comentario, genero "
                                         + "FROM spoiler sp, comentario c, usuario us, pelicula p "
                                         + "WHERE sp.id_spoiler = c.id_spoiler AND us.id_usuario = c.id_usuario AND sp.id_pelicula = p.id_pelicula; ");
