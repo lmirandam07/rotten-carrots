705e54b913123d03a739c4fcd7ff8df825d084a9
Luis <luyimiranda07@gmail.com>
2020-07-24 21:48:49 -0500

    Css icono de usuario

--- src/java/Proceso/ContactoProceso.java
+++ src/java/Proceso/ContactoProceso.java
@@ -29,16 +29,7 @@ public class ContactoProceso {
             Class.forName("org.mariadb.jdbc.Driver");
           
             /*  Es para igualar la variable conn, con los datos de la base de datos a la que nos estamos conectando, necesita 3 parametros: ruta, usuario y contraseña   */
-<<<<<<< HEAD
             this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "12345");
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> leo
-            this.conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/rotten_carrots", "root", "12345");
-
->>>>>>> 66a655cd8b43c029a5a342894c0acfe64227aaae
 
             /*  para tirar el error generico de que no se conecto a la base de datos   */
         }catch(SQLException e){
